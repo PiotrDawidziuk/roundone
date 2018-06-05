049183ecb2fc05a5f153c240451b3b585790bb0e
PiotrDawidziuk <piotr.k.dawidziuk@gmail.com>
2018-06-05 22:41:30 +0200

    class and controller starded

---
+++ src/main/java/pl/piotrdawidziuk/roundone/controller/PracticeController.java
@@ -0,0 +1,11 @@
+package pl.piotrdawidziuk.roundone.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class PracticeController {
+
+    
+
+
+}
---
+++ src/main/java/pl/piotrdawidziuk/roundone/model/Practice.java
@@ -0,0 +1,16 @@
+package pl.piotrdawidziuk.roundone.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table (name = "practices")
+public class Practice {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    long id;
+
+    String day;
+
+
+}
