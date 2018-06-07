e4e160f47e1ad4cc4db3ea2a8ff790c62ff837dd
PiotrDawidziuk <piotr.k.dawidziuk@gmail.com>
2018-06-07 22:50:57 +0200

    user controller updated

--- src/main/java/pl/piotrdawidziuk/roundone/controller/UserController.java
+++ src/main/java/pl/piotrdawidziuk/roundone/controller/UserController.java
@@ -4,9 +4,13 @@ package pl.piotrdawidziuk.roundone.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import pl.piotrdawidziuk.roundone.model.User;
 import pl.piotrdawidziuk.roundone.repository.UserRepository;
+
+import javax.validation.Valid;
 
 @Controller
 public class UserController {
@@ -15,10 +19,23 @@ public class UserController {
     UserRepository userRepository;
 
     @GetMapping("/newuser")
-    String newUser (Model model) {
+    String newUserGet (Model model) {
         model.addAttribute("user", new User());
 
         return "form/newuser";
+    }
+
+    @PostMapping("/newuser")
+    String newUserPost (@Valid User user, BindingResult result){
+        if (result.hasErrors()){
+            return "form/newuser"; // add new user form
+        }
+
+        else {
+            userRepository.save(user);
+            long id = user.getId();
+            return "redirect:/practice/"+id; //add JSP for adding practices and practice controller
+        }
     }
 
 
--- src/main/java/pl/piotrdawidziuk/roundone/model/User.java
+++ src/main/java/pl/piotrdawidziuk/roundone/model/User.java
@@ -13,13 +13,33 @@ public class User {
 
     String username;
 
-
-
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
     List<Practice> practices;
 
+    public User() {
+    }
 
+    public long getId() {
+        return id;
+    }
 
+    public void setId(long id) {
+        this.id = id;
+    }
 
+    public String getUsername() {
+        return username;
+    }
 
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public List<Practice> getPractices() {
+        return practices;
+    }
+
+    public void setPractices(List<Practice> practices) {
+        this.practices = practices;
+    }
 }
